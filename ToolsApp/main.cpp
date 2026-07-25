b7c93cb0c98ded58de0c183e8a5470eb8f8a3c7e
atariki <loonar@yandex.ru>
2026-07-25 15:13:11 +0300

    Commit #0012 - Add 6502 disassembler test

--- ToolsApp/main.cpp
+++ ToolsApp/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 
 #include <AtariStudio/Core/Memory.h>
-#include <AtariStudio/Disassembler/Disassembler.h>
 #include <AtariStudio/Disassembler/Listing.h>
 
 int main()
@@ -12,19 +11,51 @@ int main()
 
     atari::Memory memory;
 
-    std::cout << "Memory successfully created.\n";
+    //
+    // Test program at $2000:
+    //
+    // 2000: A9 44      LDA #$44
+    // 2002: 8D 00 D0   STA $D000
+    // 2005: A2 10      LDX #$10
+    // 2007: CA         DEX
+    // 2008: D0 FD      BNE $2007
+    // 200A: 60         RTS
+    //
+
+    memory.Write8(0x2000, 0xA9);
+    memory.Write8(0x2001, 0x44);
+
+    memory.Write8(0x2002, 0x8D);
+    memory.Write8(0x2003, 0x00);
+    memory.Write8(0x2004, 0xD0);
+
+    memory.Write8(0x2005, 0xA2);
+    memory.Write8(0x2006, 0x10);
+
+    memory.Write8(0x2007, 0xCA);
+
+    memory.Write8(0x2008, 0xD0);
+    memory.Write8(0x2009, 0xFD);
+
+    memory.Write8(0x200A, 0x60);
 
     atari::Listing listing;
 
-    std::cout << "Listing engine successfully created.\n";
+    const auto instructions =
+        listing.Build(
+            memory,
+            0x2000,
+            0x200A);
 
-    atari::Disassembler disassembler;
+    const auto lines =
+        listing.Format(instructions);
 
-    std::cout << "Disassembler successfully created.\n";
+    std::cout << "Disassembly:\n\n";
 
-    std::cout << "\nInitialization completed successfully.\n";
-
-    std::cout << "\nInitialization completed successfully.\n";
+    for (const auto& line : lines)
+    {
+        std::cout << line << '\n';
+    }
 
     std::cout << "\nPress Enter to exit...";
     std::cin.get();
